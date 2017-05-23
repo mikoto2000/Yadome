d7ca4bc7946baed24d00f7ca14bf157fb61bb04f
mikoto2000 <mikoto2000@gmail.com>
2017-05-23 23:12:32 +0900

    Improvemented TreeItem display.
    refs #5.
    
    Support this nodes:
    
    - element
    - comment
    - cdata
    
    other nodes display `[ tagName : textContent ]`.

--- src/main/java/jp/dip/oyasirazu/yadome/YadomeViewData.java
+++ src/main/java/jp/dip/oyasirazu/yadome/YadomeViewData.java
@@ -1,8 +1,12 @@
 package jp.dip.oyasirazu.yadome;
 
+import java.lang.StringBuilder;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import org.w3c.dom.Attr;
+import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 
 /**
@@ -16,12 +20,35 @@ public class YadomeViewData {
     @Override
     public String toString() {
         String str;
-        if (node.getNodeType() == Node.TEXT_NODE) {
-            return "[ " + node.getNodeName() + " : "
-                    + node.getTextContent() + " ]";
-        } else {
-            return "[" + node.getNodeName() + "]";
+        switch (node.getNodeType()) {
+            case Node.ELEMENT_NODE:
+                return buildAttributeString(node);
+            case Node.COMMENT_NODE:
+                return "<!--" + node.getTextContent() + "-->";
+            case Node.CDATA_SECTION_NODE:
+                return "<![CDATA[" + node.getTextContent() + "]]>";
+            default:
+                return "[ " + node.getNodeName() + " : "
+                        + node.getTextContent() + " ]";
         }
+    }
+
+    /**
+     * Node の情報から、 `<tagName attributeName="value">` の文字列を組み立てる。
+     */
+    private static String buildAttributeString(Node node) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("<" + node.getNodeName());
+
+        NamedNodeMap attrs = node.getAttributes();
+        int loopLength = attrs.getLength();
+        for (int i = 0; i < loopLength; i++) {
+            Attr attr = (Attr) attrs.item(i);
+            sb.append(" " + attr.getName() + "=\"" + attr.getValue() + "\"");
+        }
+        sb.append(">");
+
+        return sb.toString();
     }
 }
 
